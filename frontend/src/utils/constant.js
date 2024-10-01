e1e410db175082d45917be05e66fe26ee448203e
drAJaysvit <109869929+drAJaysvit@users.noreply.github.com>
2024-10-01 18:42:24 +0530

    ff

---
+++ frontend/src/utils/constant.js
@@ -0,0 +1,6 @@
+const serverUrlOnline = "https://jobportal-server2.onrender.com"
+const serverUrlOffline = "http://localhost:3000"
+export const USER_API_END_POINT=`${serverUrlOnline}/api/v1/user`;
+export const JOB_API_END_POINT=`${serverUrlOnline}/api/v1/job`;
+export const APPLICATION_API_END_POINT=`${serverUrlOnline}/api/v1/application`;
+export const COMPANY_API_END_POINT=`${serverUrlOnline}/api/v1/company`;
